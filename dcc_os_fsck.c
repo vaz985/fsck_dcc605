b1cad9d014a22e7076c68416203ae96cde2fbf6b
artur vaz <vaz985@gmail.com>
2017-10-29 21:40:17 -0200

    mout check, superblock decoder, group decoder

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -13,16 +13,21 @@
 #define BASE_OFFSET 1024
 #define BLOCK_OFFSET(block) (BASE_OFFSERT + (block-1)*block_size)
 
+struct ext2_super_block sb;
+struct ext2_group_desc group_descr;
+
 int main(int agrc, const char * argv[]) {
   int fd;
   fd = open(argv[1], O_RDWR);
 
   // Read and decode the superblock
-  struct ext2_super_block sb;
   lseek(fd, BASE_OFFSET, SEEK_SET);
   read(fd, &sb, sizeof(sb));
   /////////////////////////////////
-
+  // Read and decode the group descriptor
+  int group_size = BASE_OFFSET << sb.s_log_block_size;
+  read(fd, &group_descr, SEEK_SET); 
+  
   if( mount(argv[1], "/tmp", "ext2", MS_RDONLY, "")== -1 ) { 
     printf("erro no mount, recuperar superblock");
     
