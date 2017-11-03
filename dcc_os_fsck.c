4648afe5f8a217f7d86227916e047f4569afcafd
artur vaz <vaz985@gmail.com>
2017-11-02 22:54:59 -0200

    superblock fix done

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -8,13 +8,12 @@
 #include <fcntl.h>
 
 #include <linux/fs.h> 
-#include <ext2fs/ext2fs.h>
+#include "ext2.h"
 
 #define BASE_OFFSET 1024
 #define BLOCK_OFFSET(block) (BASE_OFFSERT + (block-1)*block_size)
 
 struct ext2_super_block sb;
-struct ext2_group_desc group_descr;
 
 // Tornar mais brando o fix do superblock
 void superblock_fix( int fd ){
@@ -39,11 +38,6 @@ int main(int agrc, const char * argv[]) {
   fd = open(argv[1], O_RDWR);
 
   superblock_fix( fd );
-
-
-  int group_size = BASE_OFFSET << sb.s_log_block_size;
-  read(fd, &group_descr, SEEK_SET); 
-  
 
   return 0;
 }
