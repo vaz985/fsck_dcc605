18dc4d27e7f81e93c2c99882c628e931ed8a14c9
artur vaz <vaz985@gmail.com>
2017-11-03 17:27:57 -0200

    Multiple inode progess

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -13,6 +13,9 @@
 #define BASE_OFFSET 1024
 #define BLOCK_OFFSET(block) (BASE_OFFSET + (block-1)*block_size)
 
+unsigned int bitmapGet( char byte, unsigned int pos ) {
+  return (byte >> pos) & 1;
+}
 
 // Tornar mais brando o fix do superblock
 void superblock_fix( int fd ){
@@ -24,7 +27,7 @@ void superblock_fix( int fd ){
     return;
   }
   
-  lseek(fd, BASE_OFFSET*8193, SEEK_SET);
+  lseek(fd, 1024*8193, SEEK_SET);
   read(fd, &super, sizeof(super));
 
   lseek(fd, BASE_OFFSET, SEEK_SET);
@@ -35,25 +38,38 @@ void superblock_fix( int fd ){
 void multiple_inode( int fd ) {
   struct ext2_super_block super;
   struct ext2_group_desc group_descr;
+  struct ext2_inode inode;
 
-
-  lseek(fd, BASE_OFFSET, sizeof(super));
+  lseek(fd, BASE_OFFSET, SEEK_SET);
   read(fd, &super, sizeof(super));
 
-  int block_size = super.s_blocksize;
+  // In bytes
+  int block_size = 1024 << super.s_log_block_size;
 
   unsigned int group_count = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group;
+  unsigned int descr_list_size = group_count * sizeof(struct ext2_group_desc);
+
   unsigned int inodes_per_block = block_size / sizeof(struct ext2_inode);
   unsigned int itable_blocks = super.s_inodes_per_group / inodes_per_block;
 
-  for( int group_i = 0; group_i < group_count; group_i++) {
-    lseek( fd, BASE_OFFSET + group_i*super.s_blocks_per_group, SEEK_SET);
-    for( int block_i = 0; block_i < (block_size-2)/3; block_i++){ 
-      read(fd, &group_descr, sizeof(group_descr));
+  lseek( fd, BASE_OFFSET + block_size, SEEK_SET);
+  read( fd, &group_descr, sizeof(struct ext2_group_desc));
 
-       
-    } 
+  unsigned int inode_bitmap = group_descr.bg_inode_bitmap;
+  unsigned int inode_table  = group_descr.bg_inode_table;
+  
+  unsigned char *bitmap = malloc(block_size);
+  lseek( fd, BLOCK_OFFSET(inode_bitmap), SEEK_SET);
+  read( fd, bitmap, sizeof(bitmap));
+
+  for( int i = 0; i < block_size; i++){
+    for( int bit = 0; i < 8; i++) {
+      if( bitmapGet(bitmap[i], bit) ) {
+         
+      }
+    }
   }
+
 }
 
 int main(int agrc, const char * argv[]) {
