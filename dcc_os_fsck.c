f035ce5851fd103453d8c8f1ad77c7433da740e6
artur vaz <vaz985@gmail.com>
2017-11-02 22:52:22 -0200

    Superblock fix done

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -16,23 +16,34 @@
 struct ext2_super_block sb;
 struct ext2_group_desc group_descr;
 
+// Tornar mais brando o fix do superblock
+void superblock_fix( int fd ){
+  
+  lseek(fd, BASE_OFFSET, SEEK_SET);
+  read(fd, &sb, sizeof(sb));
+  if( sb.s_magic == 61267 ) {
+    printf("Superblock OK\n");
+    return;
+  }
+  
+  lseek(fd, BASE_OFFSET*8193, SEEK_SET);
+  read(fd, &sb, sizeof(sb));
+
+  lseek(fd, BASE_OFFSET, SEEK_SET);
+  write(fd, &sb, sizeof(sb));
+  
+}
+
 int main(int agrc, const char * argv[]) {
   int fd;
   fd = open(argv[1], O_RDWR);
 
-  // Read and decode the superblock
-  lseek(fd, BASE_OFFSET, SEEK_SET);
-  read(fd, &sb, sizeof(sb));
-  /////////////////////////////////
-  // Read and decode the group descriptor
+  superblock_fix( fd );
+
+
   int group_size = BASE_OFFSET << sb.s_log_block_size;
   read(fd, &group_descr, SEEK_SET); 
   
-  if( mount(argv[1], "/tmp", "ext2", MS_RDONLY, "")== -1 ) { 
-    printf("erro no mount, recuperar superblock");
-    
-    exit(1);
-  }
 
   return 0;
 }
