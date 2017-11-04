bce1190f083749704c8dd9d85d5fabcbab407d0b
artv <vaz985@gmail.com>
2017-11-04 19:29:54 -0200

    search for dup refs working

--- dcc_os_fsck.c
+++ dcc_os_fsck.c
@@ -35,45 +35,84 @@ void superblock_fix( int fd ){
   
 }
 
-//static void read_inode( int fd, int inode_no, const ext2_group_desc * group, struct ext2_inode * inode) {
-//  unsigned int block_size = 1024;
-//
-//  lseek(fd, BLOCK_OFFSET(group->bg_inode_table) + (inode_no)*sizeof(struct ext2_inode), SEEK_SET);
-//  read(fd, inode, sizeof(struct ext2_inode));
-//  
-//}
+static void read_inode( int fd, int inode_no, const struct ext2_group_desc * group, struct ext2_inode * inode) {
+  unsigned int block_size = 1024;
+
+  lseek(fd, BLOCK_OFFSET(group->bg_inode_table) + (inode_no)*sizeof(struct ext2_inode), SEEK_SET);
+  read(fd, inode, sizeof(struct ext2_inode));
+  
+}
 
 void multiple_inode( int fd ) {
   struct ext2_super_block super;
-  struct ext2_group_desc  group;
-  struct ext2_group_desc  group2;
 
   lseek(fd, BASE_OFFSET, SEEK_SET);
   read(fd, &super, sizeof(super));
 
-  // In bytes
-  int block_size = 1024 << super.s_log_block_size;
-
-  unsigned int group_count = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group;
-  unsigned int descr_list_size = group_count * sizeof(struct ext2_group_desc);
+  unsigned int block_size = 1024 << super.s_log_block_size;
 
   unsigned int inodes_per_block = block_size / sizeof(struct ext2_inode);
-  unsigned int itable_blocks = super.s_inodes_per_group / inodes_per_block;
+  unsigned int itable_blocks    = super.s_inodes_per_group / inodes_per_block;
 
-  lseek( fd, BASE_OFFSET + block_size, SEEK_SET);
-  read( fd, &group, sizeof(struct ext2_group_desc));
+  unsigned int group_count      = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group;
 
-  unsigned int inode_bitmap = group.bg_inode_bitmap;
-  unsigned int inode_table  = group.bg_inode_table;
-  
-  unsigned char *bitmap      = malloc(block_size);
-  unsigned char *block_count = calloc(block_size, sizeof(unsigned char));
+  struct ext2_group_desc group[group_count];
+  lseek(fd, BASE_OFFSET + block_size, SEEK_SET);
+  read(fd, group, sizeof(group));
 
-  lseek(fd, BLOCK_OFFSET(inode_bitmap), SEEK_SET);
-  read(fd, bitmap, sizeof(bitmap));
+  unsigned int * ref;
+  struct ext2_inode inodes[inodes_per_block];
+  // Caminho por cada BLOCK GROUP
+  for(int n = 0; n < group_count; n++) {
+    ref = calloc( sizeof(unsigned int) , 8192 );
+    // Posicao no comeco da tabela de inodes
+    lseek(fd, BLOCK_OFFSET(group[n].bg_inode_table), SEEK_SET);
+    for(int i = 0; i < itable_blocks; i++) {
+      // Leio 1 block de inodes
+      read(fd, inodes, sizeof(inodes));
+      for(int j = 0; j < inodes_per_block; j++) {
+        unsigned int b_count    = inodes[j].i_blocks;
+        unsigned int * blocks = inodes[j].i_block;
+        // Se o inode tem algo
+        if( b_count > 0 ) {
+          for( int k = 0; k < 12; k++ ) {
+            if( blocks[k] == 0 ) {
+            }
+            else if( ref[blocks[k]] == 0 ) {
+              ref[blocks[k]]++;
+            }
+            // Bloco com mais de 1 ref
+            else {
+              printf("Bloco %d com mais de 1 ref\n",blocks[k]);
+            }
+          }
+        }
+      }
+    }
+    free(ref);
+  }
+  //// In bytes
 
-  struct ext2_inode * inode = malloc( sizeof(struct ext2_inode) );
+  //unsigned int descr_list_size = group_count * sizeof(struct ext2_group_desc);
+
+  //unsigned int inodes_per_block = block_size / sizeof(struct ext2_inode);
+  //unsigned int itable_blocks = super.s_inodes_per_group / inodes_per_block;
+
+  //lseek( fd, BASE_OFFSET + block_size, SEEK_SET);
+  //read( fd, &group, sizeof(struct ext2_group_desc));
+
+  //unsigned int inode_bitmap = group.bg_inode_bitmap;
+  //unsigned int inode_table  = group.bg_inode_table;
+  //
+  //unsigned char *bitmap      = malloc(block_size);
+  //unsigned char *block_count = calloc(block_size, sizeof(unsigned char));
+
+  //lseek(fd, BLOCK_OFFSET(inode_bitmap), SEEK_SET);
+  //read(fd, bitmap, sizeof(bitmap));
+
+  //struct ext2_inode * inode = malloc( sizeof(struct ext2_inode) );
   //read_inode( fd, 1, &group, inode);
+
 
   //for(int i = 0; i < 1024; i++) {
   //  if( bitmap[i] > 0 ) {
